cc2b02451d5a05e56869dd3d3f3d7869688e22fd
guhdalla <guhdalla011@gmail.com>
2022-03-04 14:23:52 -0300

    Inicial

---
+++ src/main/java/com/gamerloot/GamerlootApplication.java
@@ -0,0 +1,13 @@
+package com.gamerloot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerlootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerlootApplication.class, args);
+	}
+
+}
